ded514770886745b447e787506d872e8c2c5feeb
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2019-10-14 18:38:38 -0400

    Add Header JS

---
+++ assets/js/src/c-header.js
@@ -0,0 +1,12 @@
+// $( window ).scroll( function () {
+// 	if ($( this ).scrollTop() > 30 ) {
+// 		$( '.c-header' ).addClass( 'c-header--scroll' );
+// 		$('.c-header_logo-img').addClass('c-header_logo-img--scroll' );
+// 	} else {
+// 		$( '.c-header' ).removeClass( 'c-header--scroll' );
+// 		$( '.c-header_logo-img' ).removeClass( 'c-header_logo-img--scroll' );
+// 	}
+// });
+// document.addEventListener('DOMContentLoaded', function() {
+// 	console.log( 'header JS Loaded' );
+// } );
