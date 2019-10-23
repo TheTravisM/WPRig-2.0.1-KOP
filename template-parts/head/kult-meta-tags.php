e609cfd861b33c29ca9749d943797232ab20294a
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2019-10-23 12:49:12 -0400

    Add Best Pratice stuff

--- template-parts/head/kult-meta-tags.php
+++ template-parts/head/kult-meta-tags.php
@@ -11,6 +11,7 @@ namespace WP_Rig\WP_Rig;
 
 <!-- [ KULT Meta Tags ] -->
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="description" content="Kult of Pop is the place to find all things pop culture. Stop by read, listen, and drink some Kool-Aid!">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 <meta property="og:site_name" content="Kult Of Pop">
 <meta property="og:type" content="website">
@@ -18,3 +19,4 @@ namespace WP_Rig\WP_Rig;
 <meta property="og:locale" content="en">
 <meta property="og:title" content="Kult Of Pop">
 <meta name="referrer" content="strict-origin">
+<link rel="canonical" href="https://kultofpop.com"/>
